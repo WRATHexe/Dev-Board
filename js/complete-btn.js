018e978d161315c6a222fbfe189fb87014303820
WRATHeXe <FardinRuhian001@gmail.com>
2025-03-01 23:28:43 +0600

    added backend features

---
+++ js/complete-btn.js
@@ -0,0 +1,56 @@
+    let buttons = document.getElementsByClassName("complete-btn");
+    let activityLog = getElement("activityLog");
+    let clearHistory =getElement("clearHistory");
+    for (let btn of buttons) {
+        btn.addEventListener("click", function () {
+            this.disabled = true;
+            this.classList.add("opacity-70");
+
+            let taskName = this.parentElement.parentElement.querySelector("h3").innerText;
+            alert("You have completed the task: " + taskName);
+            //handles task completion update
+            const completeTask = getElement("completed-task");
+            completeTask.innerText = parseInt(completeTask.innerText) + 1;
+            const remainingTask = getElement("pending-task");
+            remainingTask.innerText = parseInt(remainingTask.innerText) - 1;
+            //handles activity log
+            let time = new Date().toLocaleTimeString();
+            let p = document.createElement("p");
+            p.classList.add("bg-gray-200", "p-2", "rounded-lg", "text-sm");
+            p.innerText = `You have completed the task "${taskName}" at ${time}`;
+            activityLog.appendChild(p);
+
+            let allDisabled = [...buttons].every(button => button.disabled);
+            if (allDisabled) {
+                alert("You have completed all your tasks!");
+            }
+            clearHistory.addEventListener("click", function () {
+                activityLog.innerHTML = "";
+            });
+        });
+    }
+
+    function getElement(id)
+    {
+        return document.getElementById(id);
+    }
+
+    function updateDateTime(){
+        let now=new Date();
+        
+        let dayName = now.toLocaleString("en-US", { weekday: "short" });
+        let dateString = now.toLocaleDateString("en-US", { month: "short", day: "numeric", year: "numeric" });
+
+        document.getElementById("day").innerText = `${dayName},`;
+        document.getElementById("date").innerText = dateString;
+    }
+    setInterval(updateDateTime, 1000);
+    updateDateTime();
+
+    function getRandomColor() {
+        return `#${Math.floor(Math.random() *256*256*256).toString(16)}`;
+    }
+
+    document.getElementById("theme").addEventListener("click", function(){
+        document.body.style.backgroundColor = getRandomColor(); 
+    });
